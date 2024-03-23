0aaad98440017aad5b19dbb6978d9e734c6b9fc6
Ajay Kumar Tyagi <151626208+ajaytyagi7@users.noreply.github.com>
2024-03-23 10:13:25 +0530

    update

--- Frontend/src/Component/Navbar.jsx
+++ Frontend/src/Component/Navbar.jsx
@@ -12,6 +12,9 @@ const Navbar = () => {
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+     
+      </ul>
+      <ul  className="navbar-nav me-auto mb-2 mb-lg-0">
       <li className="nav-item">
                 <NavLink className="nav-link" to="/Home">
                   Home
