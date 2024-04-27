4c7f53464508910d1f622e0714bffcddb4a1ad0d
Ajay Kumar Tyagi <151626208+ajaytyagi7@users.noreply.github.com>
2024-04-27 23:11:56 +0530

    update

--- Frontend/src/Component/About.jsx
+++ Frontend/src/Component/About.jsx
@@ -3,15 +3,14 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <div className='row'>
-        <div className="col-md-7">
-          <h3>About Us</h3>
+      <div className='row p-5'>
+        <div className="col-md-4">
+          <h3 className='text-center'>About Us</h3>
           <p>Our website is a platform where you can buy and sell products. You can also add products to your wishlist and cart. You can also view your order history. You can also contact us for any queries.</p>
 
         </div>
-        <div className="col-md-5">
-          <h3>Our Mission</h3>
-          <p>Our mission is to provide the best quality products to our customers. We aim to provide a seamless experience to our customers.</p>
+        <div className="col-md-7">
+          <img className='img-fluid' src="https://img.freepik.com/free-vector/business-team-brainstorm-idea-lightbulb-from-jigsaw-working-team-collaboration-enterprise-cooperation-colleagues-mutual-assistance-concept-pinkish-coral-bluevector-isolated-illustration_335657-1651.jpg?t=st=1714238654~exp=1714242254~hmac=5dfae91d35b8172cac48045202a9cc2d0ebbced1022192c0a402bdccd4340f67&w=996" alt="" />
         </div>
       </div>
     </div>
